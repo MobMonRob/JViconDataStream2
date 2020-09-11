3317e0ecf2ef37e947638cee25680008f52ffa91
codedeedope <26957536+codedeedope@users.noreply.github.com>
2020-09-12 00:03:27 +0200

    kritischen Fehler behoben: Linux.patch wird jetzt berücksichtigt

--- swig/invoke.sh
+++ swig/invoke.sh
@@ -7,8 +7,9 @@ cd "$(dirname "$BASH_SOURCE")"
 mkdir -p "../JViconDataStream/src/main/java/de/dhbw/rahmlab/vicon/datastream/api/impl/"
 
 cat PlatformIndependent.i >> genLinux.i
-cat Windows.patch >> genLinux.i
+cat Linux.patch >> genLinux.i
 
+#-debug-tmsearch
 swig -Wall -c++ -doxygen -java -package de.dhbw.rahmlab.vicon.datastream.api.impl -outdir "../JViconDataStream/src/main/java/de/dhbw/rahmlab/vicon/datastream/api/impl" -o ViconDataStreamSDK_wrap.cpp genLinux.i
 echo "SWIG finished"
 
