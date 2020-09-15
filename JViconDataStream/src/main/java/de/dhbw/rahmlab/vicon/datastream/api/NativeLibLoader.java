be717d5ee262ad809faf652c33c476aa33c097cd
Oliver Rettig <Oliver.Rettig@orat.de>
2020-09-15 08:46:49 +0200

    pom.xml und NativeLibLoader keine wichtigen Änderungen

--- JViconDataStream/src/main/java/de/dhbw/rahmlab/vicon/datastream/api/NativeLibLoader.java
+++ JViconDataStream/src/main/java/de/dhbw/rahmlab/vicon/datastream/api/NativeLibLoader.java
@@ -1,38 +1,36 @@
 package de.dhbw.rahmlab.vicon.datastream.api;
 
-//import com.nativeutils.NativeUtils;
-//import java.io.IOException;
+import com.nativeutils.NativeUtils;
+import java.io.IOException;
 
 /**
- * Testweise wieder auskommentiert.
- * 
  * @author fabian
  */
 public class NativeLibLoader {
 
-	private static boolean isLoaded = false;
+    private static boolean isLoaded = false;
 
-	public static void load() {
-		if (!isLoaded) {
-			loadActually();
-			isLoaded = true;
-		}
-	}
+    public static void load() {
+        if (!isLoaded) {
+            loadActually();
+            isLoaded = true;
+        }
+    }
 
-	private static void loadActually() {
-            System.loadLibrary("jViconDataStreamSDK");
-        
-            /*try {
-                    NativeUtils.loadLibraryFromJar("/lib/linux-x86_64/libjViconDataStreamSDK.so");
-            } catch (IOException e) {
-                    System.err.println("Native code library failed to load. \n" + e);
-                    System.exit(1);
-            }*/
-	}
+    private static void loadActually() {
+        System.loadLibrary("jViconDataStreamSDK");
 
-	/*
-	static {
-		NativeLibLoader.load();
-	}
-	 */
+        try {
+                NativeUtils.loadLibraryFromJar("/lib/linux-x86_64/libjViconDataStreamSDK.so");
+        } catch (IOException e) {
+                System.err.println("Native code library failed to load. \n" + e);
+                System.exit(1);
+        }
+    }
+
+    /*
+    static {
+            NativeLibLoader.load();
+    }
+     */
 }
