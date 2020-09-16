4cdc14c4e27864c2799e1ab63a1fcb2547feca5f
codedeedope <26957536+codedeedope@users.noreply.github.com>
2020-09-16 17:36:37 +0200

    clear, gitignore verbessert

--- clear.sh
+++ clear.sh
@@ -2,7 +2,10 @@
 
 cd "$(dirname "$BASH_SOURCE")"
 
-#remove ignored files and folders
-git ls-files -c -o -i --exclude-standard --directory -z| xargs -0 rm -r &> /dev/null
+#remove untracked AND ignored files and folders
+git clean -d -f -X &> /dev/null
+
+#remove ignored tracked files and folders
+git ls-files -i --exclude-standard --directory -z| xargs -0 rm -r &> /dev/null
 
 echo "JViconDataStream2 cleared"
--- clear_hard.sh
+++ clear_hard.sh
@@ -3,9 +3,9 @@
 cd "$(dirname "$BASH_SOURCE")"
 
 #remove untracked files and folders
-git clean -d -f &> /dev/null
+git clean -d -f -x &> /dev/null
 
-#remove ignored files and folders
-git ls-files -c -o -i --exclude-standard --directory -z| xargs -0 rm -r &> /dev/null
+#remove ignored tracked files and folders
+git ls-files -i --exclude-standard --directory -z| xargs -0 rm -r &> /dev/null
 
-echo "JViconDataStream2 cleared"
+echo "JViconDataStream2 cleared hard"
