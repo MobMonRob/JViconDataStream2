c10f294d2a90ad6b8667f088220e1aa8807f0298
codedeedope <26957536+codedeedope@users.noreply.github.com>
2020-10-07 19:51:16 +0200

    #13: improved link

--- Linux64/generate.sh
+++ Linux64/generate.sh
@@ -6,7 +6,7 @@ cd "$(dirname "$BASH_SOURCE")"
 
 ../swig/generate.sh
 
-#https://stackoverflow.com/questions/54373254/load-dependent-so-from-other-shared-library-via-jni/58842312#58842312
+#https://web.archive.org/web/20201007174747/https://stackoverflow.com/questions/54373254/load-dependent-so-from-other-shared-library-via-jni/58842312#58842312
 gcc -shared -fPIC -cpp -std=c++17 -O3 -flto ../swig/ViconDataStreamSDK_wrap.cpp -LDataStreamSDK_1.10 -lViconDataStreamSDK_CPP -o libjViconDataStreamSDK.so -I/usr/lib/jvm/default-java/include/linux -I/usr/lib/jvm/default-java/include -Wl,-rpath,'$ORIGIN/.'
 
 echo "gcc finished"
