f90c68f25e5c8e2fe20b35df6f7eda57cc739eb9
Oliver Rettig <Oliver.Rettig@orat.de>
2020-10-08 10:33:31 +0200

    Leerzeile entfernt

--- Linux64/generate.sh
+++ Linux64/generate.sh
@@ -6,15 +6,15 @@ cd "$(dirname "$BASH_SOURCE")"
 
 ../swig/generate.sh
 
-gcc -shared -fPIC -cpp -std=c++17 -O3 -flto ../swig/ViconDataStreamSDK_wrap.cpp -LDataStreamSDK_1.10 -lViconDataStreamSDK_CPP -o libjViconDataStreamSDK.so -I/usr/lib/jvm/default-java/include/linux -I/usr/lib/jvm/default-java/include
+#https://web.archive.org/web/20201007174747/https://stackoverflow.com/questions/54373254/load-dependent-so-from-other-shared-library-via-jni/58842312#58842312
+gcc -shared -fPIC -cpp -std=c++17 -O3 -flto ../swig/ViconDataStreamSDK_wrap.cpp -LDataStreamSDK_1.10 -lViconDataStreamSDK_CPP -o libjViconDataStreamSDK.so -I/usr/lib/jvm/default-java/include/linux -I/usr/lib/jvm/default-java/include -Wl,-rpath,'$ORIGIN/.'
 
 echo "gcc finished"
 
-mkdir -p ../JViconDataStream/src/main/natives/linux-amd64/
-cp ./*.so ../JViconDataStream/src/main/natives/linux-amd64/
-cp DataStreamSDK_1.10/*.so ../JViconDataStream/src/main/natives/linux-amd64/
-# workaround
-cp DataStreamSDK_1.10/*.so.1.58.0 ../JViconDataStream/src/main/natives/linux-amd64/
+#Has to be: <Project Folder>/natives/<os-arch> !!
+mkdir -p ../JViconDataStream/natives/linux-amd64/
+cp ./*.so* ../JViconDataStream/natives/linux-amd64/
+cp ./DataStreamSDK_1.10/*.so* ../JViconDataStream/natives/linux-amd64/
 
 echo "Linux64 finished"
 
