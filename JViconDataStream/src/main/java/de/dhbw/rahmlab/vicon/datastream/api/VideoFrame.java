4423857c396d1ca5be2ad0569b87f41030ada34c
Oliver Rettig <Oliver.Rettig@orat.de>
2020-11-10 07:49:34 +0100

    VideoFrame API completed.

--- JViconDataStream/src/main/java/de/dhbw/rahmlab/vicon/datastream/api/VideoFrame.java
+++ JViconDataStream/src/main/java/de/dhbw/rahmlab/vicon/datastream/api/VideoFrame.java
@@ -2,6 +2,8 @@ package de.dhbw.rahmlab.vicon.datastream.api;
 
 import de.dhbw.rahmlab.vicon.datastream.impl.Output_GetVideoFrame;
 import de.dhbw.rahmlab.vicon.datastream.impl.VectorUchar;
+import java.util.Iterator;
+import java.util.stream.Stream;
 
 /**
  * @author Oliver Rettig (Oliver.Rettig@orat.de)
@@ -14,7 +16,8 @@ public class VideoFrame {
     private final long id;
     private final long offsetX;
     private final long offsetY;
-
+    private final VectorUchar data;
+    
     VideoFrame(Output_GetVideoFrame result) {
         height = result.getM_Height();
         width = result.getM_Width();
@@ -22,33 +25,37 @@ public class VideoFrame {
         id = result.getM_FrameID();
         offsetX = result.getM_OffsetX();
         offsetY = result.getM_OffsetY();
-
-        VectorUchar pointer = result.getM_Data();
-        //TODO
-        // vervollständigen
+        data = result.getM_Data();
     }
 
     public int getHeight() {
         return height;
     }
-
     public int getWidth() {
         return width;
     }
-
     public long getFormat() {
         return format;
     }
-
     public long getID() {
         return id;
     }
-
     public long getOffsetX() {
         return offsetX;
     }
-
     public long getOffsetY() {
         return offsetY;
+    }
+    public int size(){
+        return data.size();
+    }
+    public short get(int index){
+        return data.get(index);
+    }
+    public Iterator<Short> iterator(){
+        return data.iterator();
+    }
+    public Stream<Short> stream(){
+        return data.stream();
     }
 }
