5eb6a955173a094bcafd9e0ed0c232ce7e70d48f
codedeedope <26957536+codedeedope@users.noreply.github.com>
2020-11-13 16:01:54 +0100

    #18 imported native files

---
+++ NativeLibLoader/NativeGenerator_Linux64/generate.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cd "$(dirname "$BASH_SOURCE")"
+
+./clear.sh
+
+mkdir -p ./target/
+
+gcc -shared -fPIC -cpp -std=c++17 -O3 -flto ./src/common/*.c ./src/unix/*.c  -o libgluegen_rt.so -I/usr/lib/jvm/default-java/include/linux -I/usr/lib/jvm/default-java/include -Wl,-rpath,'$ORIGIN/.'
+
+#-LDataStreamSDK_1.10 -lViconDataStreamSDK_CPP
+
+echo "gcc finished"
+
+#Has to be: <Project Folder>/natives/<os-arch> !!
+mkdir -p ../NativeLibLoader/natives/linux-amd64/
+cp ./target/libgluegen_rt.so ../NativeLibLoader/natives/linux-amd64/
+
+echo "NativeGenerator_Linux64 finished"
+
