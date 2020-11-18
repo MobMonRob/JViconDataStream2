7ebec7c0d0421f713e591bcd776334dc66bcd129
codedeedope <26957536+codedeedope@users.noreply.github.com>
2020-11-18 16:15:27 +0100

    #12: pull semantics: JViconDataStream

---
+++ JViconDataStream/recreate_natives.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd "$(dirname "$BASH_SOURCE")"
+
+./clear.sh
+
+../Linux64/generate.sh
+
+#Has to be: <Project Folder>/natives/<os-arch> !!
+mkdir -p ./natives/linux-amd64/
+cp ../Linux64/target/*.so* ./natives/linux-amd64/
+cp ../Linux64/DataStreamSDK_1.10/*.so* ./natives/linux-amd64/
+
+cp -R ../swig/target/java/de/ ./src/main/java/
+
+echo "recreate_natives finished"
+
