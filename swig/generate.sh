11e5c6be8e6aa9cbe8de7a93a1a31dc881dcb91a
codedeedope <26957536+codedeedope@users.noreply.github.com>
2020-12-11 18:29:37 +0100

    #3: Build.1: Umbiegen.1: SWIG.1

--- swig/generate.sh
+++ swig/generate.sh
@@ -4,13 +4,15 @@ cd $(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
 
 ./clear.sh
 
+../ViconDataStreamSDK/generate.sh
+
 mkdir -p "./target/java/de/dhbw/rahmlab/vicon/datastream/impl/"
 
 cat PlatformIndependent.i >> ./target/genLinux.i
 cat Linux.patch >> ./target/genLinux.i
 
 #-debug-tmsearch
-swig -Wall -c++ -java -package de.dhbw.rahmlab.vicon.datastream.impl -outdir "./target/java/de/dhbw/rahmlab/vicon/datastream/impl" -o ./target/ViconDataStreamSDK_wrap.cpp -I../Linux64/DataStreamSDK_1.10/ ./target/genLinux.i
+swig -Wall -c++ -java -package de.dhbw.rahmlab.vicon.datastream.impl -outdir "./target/java/de/dhbw/rahmlab/vicon/datastream/impl" -o ./target/ViconDataStreamSDK_wrap.cpp -I../ViconDataStreamSDK/target/ ./target/genLinux.i
 
 echo "swig finished"
 
