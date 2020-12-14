9c160c01176c958c3bb781732f7566edf9c40364
codedeedope <26957536+codedeedope@users.noreply.github.com>
2020-12-14 19:52:52 +0100

    #3 (indirect): fix references

--- Wrapper/regenerate_local_Linux64.sh
+++ Wrapper/regenerate_local_Linux64.sh
@@ -4,9 +4,9 @@ cd $(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
 
 ./clear_local_all.sh
 
-./generate_wrapper.sh
+./generate-wrapper_local_Linux64.sh
 
-./generate_so.sh
+./generate-so_local_Linux64.sh
 
 mkdir -p "./target/Linux64/ViconDataStreamSDK/"
 cp -L -l ../ViconDataStreamSDK/target/Linux64/*.so* ./target/Linux64/ViconDataStreamSDK/
