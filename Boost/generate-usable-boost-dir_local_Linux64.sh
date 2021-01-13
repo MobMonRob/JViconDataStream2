0553ea0c267cf6f607b9678b4ce68b7a427313d3
codedeedope <26957536+codedeedope@users.noreply.github.com>
2021-01-13 20:17:32 +0100

    #3 hopefully fixed boost generation

--- Boost/generate-usable-boost-dir_local_Linux64.sh
+++ Boost/generate-usable-boost-dir_local_Linux64.sh
@@ -10,23 +10,61 @@ rm -rdf "$newBoostDir"
 
 mkdir -p "$newBoostDir"
 
+src() {
 for libpath in `find "$oldBoostDir/libs" -maxdepth 1 -mindepth 1 -type d`; do
+	#includes_old "$libpath"
+
+	#cpp files
 	libname=$(basename $libpath)
-
-	oldIncludeDir="$libpath/include/boost"
-	if [[ -e  "$oldIncludeDir" ]]; then
-		cp -r -L -l $oldIncludeDir/* "$newBoostDir"
-		#echo "fi" &>/dev/null
-	fi
-
 	oldSrcDir="$libpath/src"
 	newSrcDir="$newBoostDir/$libname"
 	if [[ -e  "$oldSrcDir" ]]; then
 		mkdir -p "$newSrcDir"
-		cp -r -L -l $oldSrcDir/* "$newSrcDir"
+		cp -u -r -L -l $oldSrcDir/* "$newSrcDir"
 		#echo "fi" &>/dev/null
 	fi
 done
+}
+
+includes_old() {
+	local -r libpath="$1"
+
+	libname=$(basename $libpath)
+	oldIncludeDir="$libpath/include/boost"
+	if [[ -e  "$oldIncludeDir" ]]; then
+		cp -u -r -L -l $oldIncludeDir/* "$newBoostDir"
+		#echo "fi" &>/dev/null
+	fi
+
+}
+src
+
+
+includes_rekursive()
+{
+	local folder="$1"
+
+	if [[ ! -e  "$folder" ]]; then
+		return
+	fi
+
+	local folderPath=$(find "$folder" -maxdepth 1 -mindepth 1 -type d)
+
+for subFolderPath in $folderPath; do
+	local subFolderName=$(basename $subFolderPath)
+	local oldIncludeDir="$subFolderPath/include/boost/"
+
+	if [[ -e  "$oldIncludeDir" ]]; then
+		echo $oldIncludeDir
+		cp -r -L -l $oldIncludeDir/* "$newBoostDir"
+		#echo "fi" &>/dev/null
+	fi
+
+	includes_rekursive "$subFolderPath"
+done
+}
+includes_rekursive "$oldBoostDir/libs"
+
 
 echo "Boost dir generated"
 
--- Boost/regenerate_dependency-recursive_Linux64.sh
+++ Boost/regenerate_dependency-recursive_Linux64.sh
@@ -4,7 +4,9 @@ cd $(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
 
 # Dependencies bevore
 
-./regenerate_local_Linux64.sh
+# Will take too long. Without benefit. Delete ./target/ by hand if needed.
+#./regenerate_local_Linux64.sh
+./ensure_dependency-recursive_Linux64.sh
 
 echo "Boost src regenerated dependency-recursive"
 
