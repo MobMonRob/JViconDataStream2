e5f94342610e5619f4f177e84f42f8dd2fec0a6e
codedeedope <26957536+codedeedope@users.noreply.github.com>
2021-01-27 17:51:02 +0100

    #3 Wrapper looks good now

--- Wrapper/generate-so_local_Linux64.sh
+++ Wrapper/generate-so_local_Linux64.sh
@@ -9,19 +9,15 @@ echo "gcc started"
 localTmp="./target/_tmp/Linux64"
 localTarget="./target/Linux64"
 viconTarget="../ViconDataStreamSDK/target/Linux64"
-boostLibPath="../Boost/target/Linux64/lib"
 javaInclude="/usr/lib/jvm/default-java/include"
 
 #-c für nicht linken (nur .o erzeugen)
 #-shared .so muss tun, damit sicher der Fehler nicht hier liegt.
-#-flto
-gcc -c -fPIC -O3 -cpp -std=c++14 "$localTmp/ViconDataStreamSDK_wrap.cpp" \
+gcc -c -fPIC -O3 -flto -cpp -std=c++14 "$localTmp/ViconDataStreamSDK_wrap.cpp" \
 -I"$javaInclude/linux" -I"$javaInclude" -I"$viconTarget" \
 -o "$localTmp/libViconDataStreamSDK_wrap.o"
 
-boostLibs=$(find "$boostLibPath" -maxdepth 1 -mindepth 1 -type f)
-
-gcc -shared -flto -pthread "$localTmp/libViconDataStreamSDK_wrap.o" -Wl,--start-group "$viconTarget/libViconDataStreamSDK_CPP.a" $boostLibs -Wl,--end-group -o "$localTarget/libjViconDataStreamSDK.so"
+gcc -shared -flto "$localTmp/libViconDataStreamSDK_wrap.o" -L"$viconTarget" -lViconDataStreamSDK_CPP -o "$localTarget/libjViconDataStreamSDK.so"
 
 echo "gcc finished"
 
--- Wrapper/regenerate_local_Linux64.sh
+++ Wrapper/regenerate_local_Linux64.sh
@@ -2,11 +2,16 @@
 
 cd $(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
 
+targetDir="./target/Linux64"
+viconLibDir="../ViconDataStreamSDK/target/Linux64"
+
 ./clear_local_all.sh
 
 ./generate-wrapper_local_Linux64.sh
 
 ./generate-so_local_Linux64.sh
+
+cp -L -l $viconLibDir/lib* $targetDir
 
 echo "Wrapper regenerated"
 
