937c1a2faa39b4e97f4c784b85fa8b63751478b9
codedeedope <26957536+codedeedope@users.noreply.github.com>
2021-01-28 18:29:36 +0100

    reduced redundancy

--- JViconDataStream/clear_local_all.sh
+++ JViconDataStream/clear_local_all.sh
@@ -1,12 +1,1 @@
-#!/bin/bash
-
-cd $(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
-
-#remove untracked AND ignored files and folders
-git clean -d -f -X &> /dev/null
-
-#remove ignored tracked files and folders
-git ls-files -i --exclude-standard --directory -z| xargs -0 rm -r &> /dev/null
-
-echo "JViconDataStream cleared"
-
+../clear_local_all.sh
