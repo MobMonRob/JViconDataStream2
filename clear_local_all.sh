0fb9934cdaa7b0946764b690f697d55ca65bd690
codedeedope <26957536+codedeedope@users.noreply.github.com>
2021-01-28 19:19:29 +0100

    fix clear script

--- clear_local_all.sh
+++ clear_local_all.sh
@@ -10,7 +10,7 @@ source "./_bash_config.sh"
 
 run() {
 	#remove untracked AND ignored files and folders
-	git clean -d -f -X &> /dev/null
+	git clean -d -ff -X &> /dev/null
 
 	#remove ignored tracked files and folders
 	git ls-files -i --exclude-standard --directory -z| xargs -0 rm -r -f &> /dev/null
