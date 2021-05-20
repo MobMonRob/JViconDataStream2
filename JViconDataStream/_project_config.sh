7b410edbb48ca5c5ecc2fc82b27d214d716bd25d
codedeedope <26957536+codedeedope@users.noreply.github.com>
2021-05-20 20:44:42 +0200

    #20 introduced platform switch

--- JViconDataStream/_project_config.sh
+++ JViconDataStream/_project_config.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-readonly projectDir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
-readonly wrapperDir="$projectDir/../Wrapper"
+
+projectDir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+wrapperDir="$projectDir/../Wrapper"
 
 #Has to be: <Project Folder>/natives/<os-arch> !!
-readonly linuxNativesPath="./natives/linux-amd64"
-readonly windowsNativesPath="./natives/windows-amd64"
+linuxNativesPath="./natives/linux-amd64"
+windowsNativesPath="./natives/windows-amd64"
+
 if [[ "$currentPlatform" == "$platformLinux" ]]; then
-	readonly currentNativesPath="$linuxNativesPath"
+	currentNativesPath="$linuxNativesPath"
 elif [[ "$currentPlatform" == "$platformWindows" ]]; then
-	readonly currentNativesPath="$windowsNativesPath"
+	currentNativesPath="$windowsNativesPath"
 else
 	echo "Error: currentPlatform=$currentPlatform not supported."
 	exit 1
