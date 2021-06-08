60bb56e17cba386df8843d6d965e1733c382ca50
codedeedope <26957536+codedeedope@users.noreply.github.com>
2021-06-08 22:03:29 +0200

    #20 windows works now

--- JViconDataStream/src/main/java/de/dhbw/rahmlab/vicon/datastream/nativelib/NativeLibLoader.java
+++ JViconDataStream/src/main/java/de/dhbw/rahmlab/vicon/datastream/nativelib/NativeLibLoader.java
@@ -1,8 +1,7 @@
 package de.dhbw.rahmlab.vicon.datastream.nativelib;
 
 import de.dhbw.rahmlab.vicon.datastream.impl.ViconDataStreamSDKSwigJNI;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 /**
  * @author fabian
@@ -19,13 +18,31 @@ public class NativeLibLoader {
     }
 
     private static void loadActually() {
-        System.setProperty("jogamp.debug", "true");
-        System.setProperty("jogamp.verbose", "true");
-        
-        List<String> glueLibNames = new ArrayList<>();
-        glueLibNames.add("jViconDataStreamSDK");
+        de.dhbw.rahmlab.nativelibloader.api.NativeLibLoader nativeLibLoader = de.dhbw.rahmlab.nativelibloader.api.NativeLibLoader.getInstanceAndSetDebugIfFirstInvokation(true);
 
-        de.dhbw.rahmlab.nativelibloader.api.NativeLibLoader.load(glueLibNames, ViconDataStreamSDKSwigJNI.class);
+        String[] libNames = new String[0];
+
+        if (nativeLibLoader.onWindows()) {
+            libNames = new String[]{
+                "winpthread-1",
+                "gcc_s_seh-1",
+                "stdc++-6",
+                "boost",
+                "ViconDataStreamSDK_CPP",
+                "jViconDataStreamSDK"
+            };
+        } else if (nativeLibLoader.onLinux()) {
+            libNames = new String[]{
+                "boost",
+                "ViconDataStreamSDK_CPP",
+                "jViconDataStreamSDK"
+            };
+        } else {
+            System.err.println("OS not supported!");
+            System.exit(1);
+        }
+
+        nativeLibLoader.load(Arrays.asList(libNames), ViconDataStreamSDKSwigJNI.class);
     }
 }
 
