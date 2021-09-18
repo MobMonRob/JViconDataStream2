511b30599156ed17d1bd494e895d5ae2009c2db8
Oliver Rettig <Oliver.Rettig@orat.de>
2021-09-18 14:55:14 +0200

    Api for Greysale blobs finished

--- JViconDataStream/src/main/java/de/orat/rahmlab/vicon/datastream/api/GreyScaleBlob.java
+++ JViconDataStream/src/main/java/de/orat/rahmlab/vicon/datastream/api/GreyScaleBlob.java
@@ -1,13 +1,10 @@
 package de.orat.rahmlab.vicon.datastream.api;
-
 import de.dhbw.rahmlab.vicon.datastream.impl.Output_GetGreyscaleBlob;
-import de.dhbw.rahmlab.vicon.datastream.impl.VectorUint;
-import de.dhbw.rahmlab.vicon.datastream.impl.VectorVectorUchar;
 
 /**
- * TODO
+ * A Greyscale blob respresents the smallest rectangular which contains all pixels
+ * of a blob produced by a single marker.
  * 
- * - public Methoden müssen noch überlegt werden
  * - unklar ob für Tracker und/oder Nexus verfügbar ist
  * 
  * @author Oliver Rettig (Oliver.Rettig@orat.de)
@@ -16,9 +13,30 @@ public class GreyScaleBlob {
     private final Output_GetGreyscaleBlob blob;
     GreyScaleBlob(Output_GetGreyscaleBlob blob){
         this.blob = blob;
-        VectorUint posx = blob.getBlobLinePositionsX();
-        VectorUint posy = blob.getBlobLinePositionsY();
-        VectorVectorUchar  values = blob.getBlobLinePixelValues();
-        short value = values.get(0).get(0);
+    }
+    /**
+     * Get value of a single pixel.
+     * 
+     * <p>The Short is the representation of one single pixel at "index" of the given
+     * line where its color is represented by values ranging in between 
+     * 0-255.0/255 should therefore represent black/white.</p>
+     * 
+     * @param positionIndex index in the line of the pixel
+     * @param lineIndex index of the line the pixel corresponds to
+     * @return pixel value
+     */
+    public short getPixelValue(int positionIndex, int lineIndex){
+        return blob.getBlobLinePixelValues().get(lineIndex).get(positionIndex);
+    }
+    /**
+     * Get the x/y-coordinates of a pixel.
+     * 
+     * @param positionIndex defines the position inside of a line
+     * @param lineIndex defines the line position
+     * @return x/y-coordinates
+     */
+    public long[] getPixelCoordinates(int positionIndex, int lineIndex){
+        return new long[]{blob.getBlobLinePositionsX().get(positionIndex),
+            blob.getBlobLinePositionsY().get(lineIndex)};
     }
 }
