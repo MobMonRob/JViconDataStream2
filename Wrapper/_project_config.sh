b4354a8e857baf8696cfe4fa56476d3451f16c84
“codedeedope” <26957536+codedeedope@users.noreply.github.com>
2021-10-01 20:09:19 +0200

    added jdk for linux

--- Wrapper/_project_config.sh
+++ Wrapper/_project_config.sh
@@ -2,8 +2,8 @@
 
 readonly projectDir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 readonly viconDir="$projectDir/_dependencies/ViconDataStreamNative"
-readonly javaIncludeLinux="/usr/lib/jvm/default-java/include"
-readonly javaIncludeWindows="$projectDir/jdk-11.0.11+9/include"
+readonly javaIncludeLinux="$projectDir/jdk-include/java-11-openjdk-amd64/include"
+readonly javaIncludeWindows="$projectDir/jdk-include/jdk-11.0.11+9/include"
 readonly mingwLibPath1="/usr/x86_64-w64-mingw32/lib"
 
 local -r minwLibPath2Beginning="/usr/lib/gcc/x86_64-w64-mingw32"
